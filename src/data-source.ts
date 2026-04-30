a415cae0a9840e2e7e496cebcd223e9d3806dcab
Assaf Revacha <assaf.git.arm@gmail.com>
2026-04-30 14:25:35 +0300

    fix migrations

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: false,
+  logging: true,
+  entities: ['src/models/**/*.entity.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+});
