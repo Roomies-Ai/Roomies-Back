ba8825baadccf29c7bf6adee14fd4ef070f94e9a
Assaf Revacha <assaf.git.arm@gmail.com>
2026-05-01 15:24:27 +0300

    fix connection

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,31 +7,31 @@ import { AuthService } from './auth.service';
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
     @Post('/register')
-    async register(@Body() body: any, @Res() res: Response, @Req() req: Request) {
+    async register(@Body() body: any, @Res({ passthrough: true }) res: Response, @Req() req: Request) {
         const result = await this.authService.register(req, res);
         return result;
     }
 
     @Post('/google')
-    async googleLogin(@Body() body: { token: string }, @Res() res: Response, @Req() req: Request) {
+    async googleLogin(@Body() body: { token: string }, @Res({ passthrough: true }) res: Response, @Req() req: Request) {
         const result = await this.authService.googleLogin(req, res);
         return result;
     }
 
     @Post('/login')
-    async login(@Body() body: { email: string, password: string }, @Res() res: Response, @Req() req: Request) {
+    async login(@Body() body: { email: string, password: string }, @Res({ passthrough: true }) res: Response, @Req() req: Request) {
         const result = await this.authService.login(req, res);
         return result;
     }
 
     @Post('/logout')
-    async logout(@Body() body: { email: string, password: string }, @Res() res: Response, @Req() req: Request) {
+    async logout(@Body() body: { email: string, password: string }, @Res({ passthrough: true }) res: Response, @Req() req: Request) {
         const result = await this.authService.logout(req, res);
         return result;
     }
 
     @Post('/refresh')
-    async refresh(@Body() body: { email: string, password: string }, @Res() res: Response, @Req() req: Request) {
+    async refresh(@Body() body: { email: string, password: string }, @Res({ passthrough: true }) res: Response, @Req() req: Request) {
         const result = await this.authService.refresh(req, res);
         return result;
     }
