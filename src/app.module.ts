6b9038a4a9142f227db8e061c796f7fcf3d4b905
AmitMizrahii <miamit112@gmail.com>
2026-05-24 08:23:06 +0300

    added cron module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
